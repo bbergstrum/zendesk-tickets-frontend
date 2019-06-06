57dddc777886efe6ca4c4a358fa104dfcb81398e
Brad <bbergstrum@hotmail.com>
2019-06-06 23:01:21 +1000

    Implemented show/hide features, implemented show selected ticket display, refactored & cleaned up ticket list functions

--- js/tickets.js
+++ js/tickets.js
@@ -1,27 +1,69 @@
-function populateList(selectedPageId){
+//  Tickets List Logic
+
+function buildTicketList(selectedPageId){
     var url = 'http://localhost:3000/ticketsByPage/' + selectedPageId;
-    // populate tickets list
+    // grab JSON from backend
     $.getJSON(url)
-    .done(function(data){
-        $('#ticket-list').empty();
-        data.forEach(ticket => {
+    .done(function(ticketList){
+        ticketList.forEach(ticket => {
             //iterate through data and build ticket list
-            $('#ticket-list').append(
-                //append table with ticket id & subject
-                '<tr><th scope="row">' + ticket.id + '</th>' +
-                '<td>' + ticket.subject + '</td>' +
-                '<td>' + ticket.status + '</td>' +
-                '<td>' + ticket.priority + '</td>' 
-            );
+            $('#tickets-list').append(
+                buildRow(ticket)
+            ); 
         });
-    });   
+        // apply click listener to each ticket
+        $('.table-row').click(function (event){
+            // update displays
+            $('#ticket-table').hide();
+            $('#pages').hide();
+            // pass the clicked ticket into show ticket function
+            showTicket(event.target);
+        });
+    });
 };
 
-//pagination 
+
+function buildRow(ticket){
+    //apply ticket ID attribute to each row respectively
+    return  '<tr class="table-row" data-ticket-id=' + ticket.id + '>' +
+            '<th scope="row">' + ticket.id + '</th>' +
+            //fill table with ticket data
+            '<td>' + ticket.subject + '</td>' +
+            '<td>' + ticket.status + '</td>' +
+            '<td>' + ticket.priority + '</td>' +
+            '</tr>';
+};
+
+//  Single Ticket Logic
+
+function showTicket(clickedTicket){
+    //request selected ticket data from backend
+    const showTicketUrl = 'http://localhost:3000/ticket/';
+    // grab ticketId from selected ticket element
+    $.getJSON(showTicketUrl + $(clickedTicket).parent().attr('data-ticket-id')) 
+    .done(function(ticket){
+        $('#selected-ticket-subject').text(ticket.subject);
+        $('#selected-ticket-desc').text(ticket.description);
+        $('#selected-ticket-priority').text(ticket.priority);
+        $('#selected-ticket').show();
+    });  
+};
+
+//  Event Listeners
+
+$('#back').click(function (){
+    //update displays
+    $("#selected-ticket").hide();
+    $("#ticket-table").show();
+    $('#pages').show();
+});
+
+//  Pagination Logic 
+
 $('#pagination').twbsPagination({
     totalPages: 4, 
     visiblePages: 4,
     onPageClick: function (event, page) {
-        populateList(page);
+        buildTicketList(page);
     }
 });
