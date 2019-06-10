2ba5f880914eb859f25cc77e90b3c19d9468e51e
Brad <bbergstrum@hotmail.com>
2019-06-10 14:53:36 +1000

    Added error handling for JSON request to back end

--- js/tickets.js
+++ js/tickets.js
@@ -1,58 +1,76 @@
 //  Tickets List Logic
 
-function buildTicketList(selectedPageId){
+function buildTicketList(selectedPageId) {
     var url = 'http://localhost:3000/ticketsByPage/' + selectedPageId;
     // grab JSON from backend
     $.getJSON(url)
-    .done(function(ticketList){
-        $('#tickets-list').empty();
-        ticketList.forEach(ticket => {
-            //iterate through data and build ticket list
+        .done(function (ticketList) {
+            $('#tickets-list').empty();
+            ticketList.forEach(ticket => {
+                //iterate through data and build ticket list
+                $('#tickets-list').append(
+                    buildRow(ticket)
+                );
+            });
+            // apply click listener to each ticket
+            $('.table-row').click(function (event) {
+                // update displays
+                $('#ticket-table').hide();
+                $('#pages').hide();
+                // pass the clicked ticket into show ticket function
+                showTicket(event.target);
+            });
+        })
+        .fail(function (jqXHR, textStatus) {
+            console.log('getJSON request failed: ' + textStatus);
+            console.log(jqXHR);
             $('#tickets-list').append(
-                buildRow(ticket)
-            ); 
+                buildTicketError()
+            );
         });
-        // apply click listener to each ticket
-        $('.table-row').click(function (event){
-            // update displays
-            $('#ticket-table').hide();
-            $('#pages').hide();
-            // pass the clicked ticket into show ticket function
-            showTicket(event.target);
-        });
-    });
 };
 
-function buildRow(ticket){
+function buildRow(ticket) {
     //apply ticket ID attribute to each row respectively
-    return  '<tr class="table-row" data-ticket-id=' + ticket.id + '>' +
-            '<th scope="row">' + ticket.id + '</th>' +
-            //fill table with ticket data
-            '<td>' + ticket.subject + '</td>' +
-            '<td>' + ticket.status + '</td>' +
-            '<td>' + ticket.priority + '</td>' +
-            '</tr>';
+    return '<tr class="table-row" data-ticket-id=' + ticket.id + '>' +
+        '<th scope="row">' + ticket.id + '</th>' +
+        //fill table with ticket data
+        '<td>' + ticket.subject + '</td>' +
+        '<td>' + ticket.status + '</td>' +
+        '<td>' + ticket.priority + '</td>' +
+        '</tr>';
+};
+
+function buildTicketError() {
+    return '<tr>' +
+        '<td></td>' +
+        '<td>Connection to ticket viewer backend unavailable.</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td></td>' +
+        '<td>Unable to fetch tickets...</td>' +
+        '</tr>'
 };
 
 //  Single Ticket Logic
 
-function showTicket(clickedTicket){
+function showTicket(clickedTicket) {
     //request selected ticket data from backend
     const showTicketUrl = 'http://localhost:3000/ticket/';
     // grab ticketId from selected ticket element
-    $.getJSON(showTicketUrl + $(clickedTicket).parent().attr('data-ticket-id')) 
-    .done(function(ticket){
-        $('#selected-ticket-id').text('Ticket ID: ' + ticket.id);
-        $('#selected-ticket-subject').text(ticket.subject);
-        $('#selected-ticket-desc').text(ticket.description);
-        $('#selected-ticket-priority').text(ticket.priority);
-        $('#selected-ticket').show();
-    });  
+    $.getJSON(showTicketUrl + $(clickedTicket).parent().attr('data-ticket-id'))
+        .done(function (ticket) {
+            $('#selected-ticket-id').text('Ticket ID: ' + ticket.id);
+            $('#selected-ticket-subject').text(ticket.subject);
+            $('#selected-ticket-desc').text(ticket.description);
+            $('#selected-ticket-priority').text(ticket.priority);
+            $('#selected-ticket').show();
+        });
 };
 
 //  Event Listeners
 
-$('.back').click(function (){
+$('.back').click(function () {
     //update displays
     $("#selected-ticket").hide();
     $("#ticket-table").show();
@@ -62,7 +80,7 @@ $('.back').click(function (){
 //  Pagination Logic 
 
 $('#pagination').twbsPagination({
-    totalPages: 4, 
+    totalPages: 4,
     visiblePages: 4,
     onPageClick: function (event, page) {
         buildTicketList(page);
